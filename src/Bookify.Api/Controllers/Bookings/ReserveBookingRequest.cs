4eade74183eb095b422df774cf862d2036e30535
poschuler <poschuler@gmail.com>
2024-10-27 21:46:30 -0500

    docker configuration

---
+++ src/Bookify.Api/Controllers/Bookings/ReserveBookingRequest.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bookify.Api.Controllers.Bookings;
+
+public sealed record ReserveBookingRequest
+(
+    Guid ApartmentId,
+    Guid UserId,
+    DateOnly StartDate,
+    DateOnly EndDate
+);
