a15cd1c8b274191b0a0900feb3d3b9ace6cb3c49
poschuler <poschuler@gmail.com>
2024-10-28 13:21:12 -0500

    end presentation layer

--- src/Bookify.Domain/Apartments/Apartment.cs
+++ src/Bookify.Domain/Apartments/Apartment.cs
@@ -22,6 +22,10 @@ public sealed class Apartment : Entity
         Amenities = amenities;
     }
 
+    private Apartment()
+    {
+    }
+
     public Name Name { get; private set; }
 
     public Description Description { get; private set; }
